0564a7b79c283092836749711c3bd6075b438eb3
rex850503 <75346265+rex850503@users.noreply.github.com>
2022-07-01 10:13:28 +0800

    0.1.6(2022-07-01)首頁登入rwd

--- public/js/dist/main.dev.js
+++ public/js/dist/main.dev.js
@@ -1,20 +1,46 @@
 "use strict";
 
 $(document).ready(function () {
-  var filter = $(".filter").offset().top;
-  filter = filter - 100;
-  console.log(filter);
+  //計算 filter父層的高度 - header高度
+  var filter = $("#filterStoreWrap").offset().top - 100;
+  $("#loginBtn, #sideListLogin").click(function () {
+    $("#login, body").addClass("openLogin");
+
+    if ($("#sideMenu").hasClass("active")) {
+      $("#sideMenu").removeClass("active");
+    }
+  });
+  $("#login").on("click", closeLogin);
+  $("#login .close").on("click", closeLogin);
+
+  function closeLogin(e) {
+    console.log(e.target); //可以知道被點擊的目標是誰
+
+    if (this == e.target) {
+      $("#login, body").removeClass("openLogin");
+    }
+  }
+
+  ; //#loginBtn end
+
   $("#burger").click(function () {
     $("#sideMenu").addClass("active");
   });
   $("#sideMenu .close").click(function () {
     $("#sideMenu").removeClass("active");
-  });
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > filter) {
+  }); //#burger end
+
+  var filterFixed = $(window).scroll(function () {
+    if ($(this).scrollTop() >= filter) {
       $(".filter").addClass("navfixed");
     } else {
       $(".filter").removeClass("navfixed");
     }
-  });
+  }); //filterFixed end
+
+  $(window).resize(function () {
+    //計算 filter父層的高度 - header高度
+    filter = $("#filterStoreWrap").offset().top - 100;
+    filterFixed();
+  }); //window resize filterFixed end
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-    var filter = $(".filter").offset().top;
-        filter = filter - 100;
-    console.log(filter);
+    //計算 filter父層的高度 - header高度
+    var filter = $("#filterStoreWrap").offset().top - 100;
+
+    $("#loginBtn, #sideListLogin").click(function(){
+        $("#login, body").addClass("openLogin");
+
+        if($("#sideMenu").hasClass("active")){
+            $("#sideMenu").removeClass("active");
+        }
+    }) 
+
+    $("#login").on("click", closeLogin);
+    $("#login .close").on("click", closeLogin);
+
+    function closeLogin(e){
+        console.log(e.target) //可以知道被點擊的目標是誰
+
+        if(this == e.target){
+            $("#login, body").removeClass("openLogin");
+        }
+    }; //#loginBtn end
 
     $("#burger").click(function(){
         $("#sideMenu").addClass("active");
@@ -9,13 +27,23 @@ $(document).ready(function(){
 
     $("#sideMenu .close").click(function(){
         $("#sideMenu").removeClass("active");
-    })
+    }) //#burger end
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > filter){
+
+    let filterFixed = $(window).scroll(function(){
+        if($(this).scrollTop() >= filter){
             $(".filter").addClass("navfixed");
         }else{
             $(".filter").removeClass("navfixed");
         }
-    })
+    }) //filterFixed end
+
+    $(window).resize(function(){
+        //計算 filter父層的高度 - header高度
+        filter = $("#filterStoreWrap").offset().top - 100;
+
+        filterFixed();
+    });//window resize filterFixed end
+
+    
 })
