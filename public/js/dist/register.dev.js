6e9dddcf74f91f9fb5417f5c15f74a1a166a6b41
rex850503 <75346265+rex850503@users.noreply.github.com>
2022-07-03 15:57:36 +0800

    0.1.8(2022-07-02) 註冊 切板完成

--- public/js/dist/register.dev.js
+++ public/js/dist/register.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
 $(window).ready(function () {
+  var passwordtype = $("#registerPassword input");
   $(".registerNext").click(function () {
     var step = $(this).data("step");
     var registerStepIndex = $(this).parent().index();
@@ -8,5 +9,12 @@ $(window).ready(function () {
     $(this).parent().removeClass("active");
     $(step).addClass("active");
     $(".stepwrap li").eq(registerStepIndex + 1).addClass("active").siblings().removeClass("active");
+  });
+  $(".viewtext").click(function () {
+    if (passwordtype.attr("type") == "password") {
+      $(passwordtype).attr("type", "text");
+    } else {
+      $(passwordtype).attr("type", "password");
+    }
   });
 });
--- public/js/register.js
+++ public/js/register.js
@@ -1,4 +1,6 @@
 $(window).ready(function(){
+    let passwordtype = $("#registerPassword input")
+
     $(".registerNext").click(function(){
         let step = $(this).data("step");
         let registerStepIndex = $(this).parent().index();
@@ -9,5 +11,13 @@ $(window).ready(function(){
         $(step).addClass("active");
 
         $(".stepwrap li").eq(registerStepIndex + 1).addClass("active").siblings().removeClass("active");
+    })
+
+    $(".viewtext").click(function(){
+        if(passwordtype.attr("type") == "password"){   
+            $(passwordtype).attr("type","text");
+        }else{
+            $(passwordtype).attr("type","password");
+        }
     })
 })
