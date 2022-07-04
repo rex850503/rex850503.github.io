bab3c3cf86b142be122037df0d6ab1bd54df9a3e
rex850503 <75346265+rex850503@users.noreply.github.com>
2022-07-04 11:42:01 +0800

    0.2.0(2022-07-04) 註冊 輸入判斷

--- public/js/dist/register.dev.js
+++ public/js/dist/register.dev.js
@@ -4,17 +4,29 @@ $(window).ready(function () {
   var passwordtype = $("#registerPassword input");
   $(".registerNext").click(function () {
     var step = $(this).data("step");
+    var nextstep = $(this).data("nextstep");
     var registerStepIndex = $(this).parent().index();
-    console.log(registerStepIndex);
-    $(this).parent().removeClass("active");
-    $(step).addClass("active");
-    $(".stepwrap li").eq(registerStepIndex + 1).addClass("active").siblings().removeClass("active");
+
+    if (step == "#registerWay" && $("#registerWay input").val() == "") {
+      alert("請填寫電話號碼");
+    } else if (step == "#verification" && $("#verification input").val() == "") {
+      console.log($("#verification input").val());
+      alert("請填寫驗證碼");
+    } else if (step == "#registerPassword" && $("#registerPassword input").val() == "") {
+      alert("請填寫密碼");
+    } else {
+      $(this).parent().removeClass("active");
+      $(nextstep).addClass("active");
+      $(".stepwrap li").eq(registerStepIndex + 1).addClass("active").siblings().removeClass("active");
+    }
   });
   $(".viewtext").click(function () {
-    if (passwordtype.attr("type") == "password") {
-      $(passwordtype).attr("type", "text");
-    } else {
-      $(passwordtype).attr("type", "password");
+    if (passwordtype.val() != "") {
+      if (passwordtype.attr("type") == "password") {
+        $(passwordtype).attr("type", "text");
+      } else {
+        $(passwordtype).attr("type", "password");
+      }
     }
   });
 });
